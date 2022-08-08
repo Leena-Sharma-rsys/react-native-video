eb282822295d661cf0c76e343bcc3eceed79853e
leena <leena@Leena-rsis-MacBook-Pro.local>
2022-08-08 18:37:42 +0530

    added the same for android

--- web/index.js
+++ web/index.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import WebVideo from "./video.web"
 export default function WebPlayer(props) {
-
+    
     return (
         <WebVideo {...props} />
     );
